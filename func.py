48abbba4f7d7a6e122e41fde5ecbf65541dd38c0
Fares Herhar <herhar.fares@yandex.com>
2022-03-07 14:34:13 +0100

    add already parsed tweets check

---
+++ func.py
@@ -0,0 +1,16 @@
+import pickle
+
+def save_object(file_name, obj):
+    try:
+        with open(file_name, "wb") as f:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except Exception as ex:
+        print("Error during pickling object (Possibly unsupported):", ex)
+
+
+def load_object(filename):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except Exception as ex:
+        print("Error during unpickling object (Possibly unsupported):", ex)
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 if __name__ == '__main__':
     from time import sleep
+    from os import listdir
 
+    from func import save_object, load_object
     from scraper import TwitterScarper
     from parser import TelegramParser
-    from config import TWT_BEARER_TOKEN, TLGRM_BOT_TOKEN, TLGRM_CHAT_ID
+    from config import (TWT_BEARER_TOKEN, TLGRM_BOT_TOKEN,
+                        TLGRM_CHAT_ID, DATA_DIR, DATA_FILE)
+
+    # Check if the file already exists
+    if 'tweets.pickle' not in listdir(DATA_DIR):
+        save_object(DATA_FILE, [])
 
     # Create Scraper, Parser objects
     scraper = TwitterScarper(TWT_BEARER_TOKEN)
     parser = TelegramParser(TLGRM_BOT_TOKEN, TLGRM_CHAT_ID)
 
+    # Read the old tweets list
+    old_tweets = load_object(DATA_FILE)
+    new_tweets = []
+
     # Get the account Twitter ID, and put a limit for the tweets
     screen_name = 'wikileaks'
     account_id = scraper.get_twitter_id(screen_name)
-    limit = 10
+    limit = 4
 
     # Retrieve the tweets
     tweets = scraper.get_tweets(account_id, limit)
 
-    # Push the tweets to Tlegram channel
-    for tweet in tweets:
-        parser.send_message(tweet.text)
-        sleep(5)
+    # Push the tweets to The Telegram channel
+    for tweet in tweets[::-1]:
+        # Check if the code has already being passed
+        # if so, don't passed a second time
+        if tweet.id not in old_tweets:
+            new_tweets.append(tweet.id)
+            parser.send_message(tweet.text)
+            sleep(3)
+
+    # Save the new list of already parsed tweets
+    save_object(DATA_FILE, old_tweets + new_tweets)
