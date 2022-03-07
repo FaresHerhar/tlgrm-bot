2032ea7dfaa683bc8143e53ee854d2760043e35e
Fares Herhar <herhar.fares@yandex.com>
2022-03-07 17:52:07 +0100

    make the script general

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
+    # import sys.argv as argv
+    from sys import argv
     from time import sleep
     from os import listdir
 
@@ -6,24 +8,34 @@ if __name__ == '__main__':
     from scraper import TwitterScarper
     from parser import TelegramParser
     from config import (TWT_BEARER_TOKEN, TLGRM_BOT_TOKEN,
-                        TLGRM_CHAT_ID, DATA_DIR, DATA_FILE)
+                        TLGRM_CHAT_ID, DATA_DIR)
+
+
+    # Parse the arguments list
+    if len(argv) < 3:
+        print('Must provide 2 arguments')
+        exit()
+    if len(argv) > 3:
+        print('Must provide 2 arguments only')
+        exit()
+
+    screen_name = argv[1]
+    limit = int(argv[2])
 
     # Check if the file already exists
-    if 'tweets.pickle' not in listdir(DATA_DIR):
-        save_object(DATA_FILE, [])
+    if '{}.pickle'.format(screen_name) not in listdir(DATA_DIR):
+        save_object('{}/{}.pickle'.format(DATA_DIR, screen_name), [])
 
     # Create Scraper, Parser objects
     scraper = TwitterScarper(TWT_BEARER_TOKEN)
     parser = TelegramParser(TLGRM_BOT_TOKEN, TLGRM_CHAT_ID)
 
-    # Read the old tweets list
-    old_tweets = load_object(DATA_FILE)
-    new_tweets = []
-
     # Get the account Twitter ID, and put a limit for the tweets
-    screen_name = 'wikileaks'
     account_id = scraper.get_twitter_id(screen_name)
-    limit = 4
+
+    # Read the old tweets list
+    old_tweets = load_object('{}/{}.pickle'.format(DATA_DIR, screen_name))
+    new_tweets = []
 
     # Retrieve the tweets
     tweets = scraper.get_tweets(account_id, limit)
@@ -35,7 +47,11 @@ if __name__ == '__main__':
         if tweet.id not in old_tweets:
             new_tweets.append(tweet.id)
             parser.send_message(tweet.text)
-            sleep(3)
+
+            # The sleep is omportant, because when sending
+            # to many parse requests without sleep, it breaks
+            sleep(2)
 
     # Save the new list of already parsed tweets
-    save_object(DATA_FILE, old_tweets + new_tweets)
+    save_object('{}/{}.pickle'.format(DATA_DIR, screen_name),
+                old_tweets + new_tweets)
