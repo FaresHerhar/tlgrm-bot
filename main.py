e5435ee948c421a4c8b1de6a9aba3afc0e2316f8
Fares Herhar <herhar.fares@yandex.com>
2022-03-14 12:05:50 +0100

    fix cache file name

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
     limit = int(argv[2])
 
     # Check if the file already exists
-    if '{}.pickle'.format(screen_name) not in listdir(DATA_DIR):
-        save_object('{}/{}.pickle'.format(DATA_DIR, screen_name), [])
+    if '{}'.format(screen_name) not in listdir(DATA_DIR):
+        save_object('{}/{}'.format(DATA_DIR, screen_name), [])
 
     # Create Scraper, tlgrm_parser objects
     twitter_scraper = TwitterScarper(TWT_BEARER_TOKEN)
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     account_id = twitter_scraper.get_twitter_id(screen_name)
 
     # Read the old tweets list
-    old_tweets = load_object('{}/{}.pickle'.format(DATA_DIR, screen_name))
+    old_tweets = load_object('{}/{}'.format(DATA_DIR, screen_name))
     new_tweets = []
 
     # Retrieve the tweets
@@ -57,5 +57,5 @@ if __name__ == '__main__':
             sleep(3)
 
     # Save the new list of already parsed tweets
-    save_object('{}/{}.pickle'.format(DATA_DIR, screen_name),
+    save_object('{}/{}'.format(DATA_DIR, screen_name),
                 new_tweets + old_tweets)
