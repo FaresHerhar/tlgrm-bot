1e8d67b0edab30f7b4cbe47798cba22f8e06ee0f
Fares Herhar <herhar.fares@yandex.com>
2022-03-23 23:41:46 +0100

    replace local storage by supabase

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     from time import sleep
     from os import listdir, getenv
     from dotenv import load_dotenv
+    from supabase import Client, create_client
 
-    from func import save_object, load_object
     from twitter import TwitterScarper
     from tlgrm import TelegramParser
 
@@ -15,6 +15,8 @@ if __name__ == '__main__':
     BOT_TOKEN = getenv('BOT_TOKEN')
     CHAT_ID = getenv('CHAT_ID')
     DATA_DIR = getenv('DATA_DIR')
+    SUPABASE_URL = getenv("SUPABASE_URL")
+    SUPABASE_KEY = getenv("SUPABASE_KEY")
 
     # Parse the arguments list
     if len(argv) < 3:
@@ -27,9 +29,8 @@ if __name__ == '__main__':
     screen_name = argv[1]
     limit = int(argv[2])
 
-    # Check if the file already exists
-    if '{}'.format(screen_name) not in listdir(DATA_DIR):
-        save_object('{}/{}'.format(DATA_DIR, screen_name), [])
+    # Create Supabase connection
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
     # Create Scraper, parser objects
     twitter_scraper = TwitterScarper(BEARER_TOKEN)
@@ -39,7 +40,8 @@ if __name__ == '__main__':
     account_id = twitter_scraper.get_twitter_id(screen_name)
 
     # Read the old tweets list
-    old_tweets = load_object('{}/{}'.format(DATA_DIR, screen_name))
+    old_tweets = supabase.table('tweets').select('tweet_id').execute().data
+    old_tweets = [item['tweet_id'] for item in old_tweets]
     new_tweets = []
 
     # Retrieve the tweets
@@ -52,9 +54,9 @@ if __name__ == '__main__':
 
         # Check if the code has already being passed
         # if so, don't passed a second time
-        if tweet.id not in old_tweets:
+        if str(tweet.id) not in old_tweets:
             print(' Pushing Tweet: {}'.format(tweet.id), end = "\r")
-            new_tweets.append(tweet.id)
+            new_tweets.append({'twitter_id': str(account_id), 'tweet_id': str(tweet.id)})
             parser.send_message(tweet.text)
 
             # The sleep is omportant, because when sending
@@ -62,5 +64,4 @@ if __name__ == '__main__':
             sleep(3)
 
     # Save the new list of already parsed tweets
-    save_object('{}/{}'.format(DATA_DIR, screen_name),
-                new_tweets + old_tweets)
+    supabase.table('tweets').insert(new_tweets).execute()
